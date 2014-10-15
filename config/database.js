7993753f8d5ee0ae13d272e64c9685ff08b66671
Dennis Jefferson <dj@dennisjefferson.com>
2014-10-14 20:46:44 -0700

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+	// the database url to connect
+	url : 'mongodb://node:node@proximus.modulusmongo.net:27017/rEp5oquz'
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+	var variable = angular.module('variable', []);
+
+		variable.controller('mainController', 
+	function($scope) {
+
+		$scope.message = 'Were on the page';
+	});
