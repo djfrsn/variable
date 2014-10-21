fc4c4095958517899af99ad596a9c785f36911a5
Dennis Jefferson <dj@dennisjefferson.com>
2014-10-20 20:15:56 -0700

    App: implement foundation dropdowns

--- app.js
+++ app.js
@@ -86,6 +86,14 @@ variable.controller('mainController', function($scope) {
 	        }
 	    	];
     		$scope.ddMenuSelected = {};
+    		  $scope.items = [
+    "The first choice!",
+    "And another choice for you.",
+    "but wait! A third!"
+  ];
+  $scope.linkItems = {
+    "Google": "http://google.com",
+    "AltaVista": "http://altavista.com"
+  };
 });
-
 	
