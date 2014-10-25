72680ed73900b9032081e5340e47a71b6d970c75
Dennis Jefferson <dj@dennisjefferson.com>
2014-10-25 14:55:53 -0700

    Gmaps: test config

--- app.js
+++ app.js
@@ -89,7 +89,19 @@ variable.controller('mainController', function($scope) {
 
 	// Init intro image slider
 	$(".rslides").responsiveSlides(); 
+ $scope.$on('mapInitialized', function(event, map) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
 
+      map.setCenter(pos);
+    });
+  });
 });
 
 
