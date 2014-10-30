1305c40e7dd007df1284209205627a43e8082fea
Dennis Jefferson <dj@dennisjefferson.com>
2014-10-29 23:16:27 -0700

    App: add google analytics & angulartics

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // create the module and name it variable
 	// also include ngRoute for all our routing needs
-var variable = angular.module('variable', ['ngRoute', 'ngAnimate', 'mm.foundation', 'ngMap']);
+var variable = angular.module('variable', ['ngRoute', 'ngAnimate', 'mm.foundation', 'ngMap', 'angulartics', 'angulartics.google.analytics']);
 
 	// Route main navigation links
 	variable.config(function($routeProvider) 
